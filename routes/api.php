eb2146f4f780249ce95af39d8a1df72ded408997
Alex-Slawen2001 <a.shumikhin01@mail.ru>
2025-05-31 20:31:56 +0500

    сгенерил модели

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\PropertyValueController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('products')->group(function() {
-Route::get('/',[PropertyController::class,'index']);
-Route::post('/',[PropertyController::class,'store']);
-Route::get('{property}',[PropertyController::class,'show']);
-Route::put('{property}',[PropertyController::class,'update']);
-Route::delete('{property}',[PropertyController::class,'destroy']);
+Route::get('/',[ProductController::class,'index']);
+Route::post('/',[ProductController::class,'store']);
+Route::get('{property}',[ProductController::class,'show']);
+Route::put('{property}',[ProductController::class,'update']);
+Route::delete('{property}',[ProductController::class,'destroy']);
 });
 
 //CRUD для значений св-в
