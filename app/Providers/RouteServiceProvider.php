4183b75171187e7c145b4ba9d25d9164db14c520
Alex-Slawen2001 <a.shumikhin01@mail.ru>
2025-06-02 00:57:52 +0500

    убрал из маршрутов api.php, перенес все в web

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Эта строка определяет, какое пространство имён используется для маршрутов.
+     *
+     * @var string
+     */
+    protected $namespace = 'App\Http\Controllers';
+
+    /**
+     * Настройки маршрутов для приложения.
+     *
+     * @return void
+     */
+    public function map()
+    {
+        $this->mapApiRoutes();
+        $this->mapWebRoutes();
+    }
+
+    /**
+     * Настройка маршрутов API.
+     *
+     * @return void
+     */
+    protected function mapApiRoutes()
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
+    }
+
+    /**
+     * Настройка маршрутов для веб-приложения.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/web.php'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
-
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\PropertyValueController;
 use Illuminate\Support\Facades\Route;
+Route::prefix('api')->group(function() {
+    Route::prefix('products')->group(function() {
+        Route::get('/',[ProductController::class,'index']);
+        Route::post('/',[ProductController::class,'store']);
+        Route::get('{property}',[ProductController::class,'show']);
+        Route::put('{property}',[ProductController::class,'update']);
+        Route::delete('{property}',[ProductController::class,'destroy']);
+    });
+});
+//CRUD для значений св-в
+Route::prefix('property-values')->group(function(){
+    Route::get('/',[PropertyValueController::class,'index']);
+    Route::post('/',[PropertyValueController::class,'store']);
+    Route::get('{property_value}',[PropertyValueController::class,'show']);
+    Route::put('{property_value}',[PropertyValueController::class,'update']);
+    Route::delete('{property_value}',[PropertyValueController::class,'destroy']);
 
-Route::get('/', function () {
-    return view('welcome');
+
 });
